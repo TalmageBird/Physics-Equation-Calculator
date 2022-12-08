1f4a2075943120598443499b0cce614cf7caefee
Talmage Bird <talmagebird@gmail.com>
2022-12-08 01:15:10 +0000

    third

--- PhysicsEquations.js
+++ PhysicsEquations.js
@@ -1,4 +1,4 @@
-document.getElementById('submit1').addEventListener('click',(e) => FMA(e))
+document.getElementById('submit1').addEventListener('click',(e) => FMA(e));
 
 function FMA(e) {
     e.preventDefault();
@@ -9,7 +9,7 @@ function FMA(e) {
     document.getElementById('fmaf').innerHTML = element;
 }
 
-document.getElementById('submit2').addEventListener('click',(e) => FMAM(e))
+document.getElementById('submit2').addEventListener('click',(e) => FMAM(e));
 
 function FMAM(e) {
     e.preventDefault();
@@ -20,7 +20,7 @@ function FMAM(e) {
     document.getElementById('fmam').innerHTML = element;
 }
 
-document.getElementById('submit3').addEventListener('click',(e) => FMAA(e))
+document.getElementById('submit3').addEventListener('click',(e) => FMAA(e));
 
 function FMAA(e) {
     e.preventDefault();
@@ -31,7 +31,7 @@ function FMAA(e) {
     document.getElementById('fmaa').innerHTML = element;
 }
 
-document.getElementById('submit4').addEventListener('click',(e) => K1(e))
+document.getElementById('submit4').addEventListener('click',(e) => K1(e));
 
 function K1(e) {
     e.preventDefault();
@@ -43,7 +43,7 @@ function K1(e) {
     document.getElementById('k1').innerHTML = element;
 }
 
-document.getElementById('submit5').addEventListener('click',(e) => K2(e))
+document.getElementById('submit5').addEventListener('click',(e) => K2(e));
 
 function K2(e) {
     e.preventDefault();
@@ -55,7 +55,7 @@ function K2(e) {
     document.getElementById('k2').innerHTML = element;
 }
 
-document.getElementById('submit6').addEventListener('click',(e) => K3(e))
+document.getElementById('submit6').addEventListener('click',(e) => K3(e));
 
 function K3(e) {
     e.preventDefault();
@@ -67,7 +67,7 @@ function K3(e) {
     document.getElementById('k3').innerHTML = element;
 }
 
-document.getElementById('submit7').addEventListener('click',(e) => K4(e))
+document.getElementById('submit7').addEventListener('click',(e) => K4(e));
 
 function K4(e) {
     e.preventDefault();
@@ -79,7 +79,7 @@ function K4(e) {
     document.getElementById('k4').innerHTML = element;
 }
 
-document.getElementById('submit8').addEventListener('click',(e) => K5(e))
+document.getElementById('submit8').addEventListener('click',(e) => K5(e));
 
 function K5(e) {
     e.preventDefault();
@@ -91,7 +91,7 @@ function K5(e) {
     document.getElementById('k5').innerHTML = element;
 }
 
-document.getElementById('submit9').addEventListener('click',(e) => K6(e))
+document.getElementById('submit9').addEventListener('click',(e) => K6(e));
 
 function K6(e) {
     e.preventDefault();
@@ -103,7 +103,7 @@ function K6(e) {
     document.getElementById('k6').innerHTML = element;
 }
 
-document.getElementById('submit10').addEventListener('click',(e) => K7(e))
+document.getElementById('submit10').addEventListener('click',(e) => K7(e));
 
 function K7(e) {
     e.preventDefault();
@@ -115,7 +115,7 @@ function K7(e) {
     document.getElementById('k7').innerHTML = element;
 }
 
-document.getElementById('submit11').addEventListener('click',(e) => K8(e))
+document.getElementById('submit11').addEventListener('click',(e) => K8(e));
 
 function K8(e) {
     e.preventDefault();
@@ -127,7 +127,7 @@ function K8(e) {
     document.getElementById('k8').innerHTML = element;
 }
 
-document.getElementById('submit12').addEventListener('click',(e) => K9(e))
+document.getElementById('submit12').addEventListener('click',(e) => K9(e));
 
 function K9(e) {
     e.preventDefault();
@@ -139,7 +139,7 @@ function K9(e) {
     document.getElementById('k9').innerHTML = element;
 }
 
-document.getElementById('submit13').addEventListener('click',(e) => K10(e))
+document.getElementById('submit13').addEventListener('click',(e) => K10(e));
 
 function K10(e) {
     e.preventDefault();
@@ -151,7 +151,7 @@ function K10(e) {
     document.getElementById('k10').innerHTML = element;
 }
 
-document.getElementById('submit14').addEventListener('click',(e) => K11(e))
+document.getElementById('submit14').addEventListener('click',(e) => K11(e));
 
 function K11(e) {
     e.preventDefault();
@@ -163,7 +163,7 @@ function K11(e) {
     document.getElementById('k11').innerHTML = element;
 }
 
-document.getElementById('submit15').addEventListener('click',(e) => K12(e))
+document.getElementById('submit15').addEventListener('click',(e) => K12(e));
 
 function K12(e) {
     e.preventDefault();
@@ -175,7 +175,7 @@ function K12(e) {
     document.getElementById('k12').innerHTML = element;
 }
 
-document.getElementById('submit16').addEventListener('click',(e) => K13(e))
+document.getElementById('submit16').addEventListener('click',(e) => K13(e));
 
 function K13(e) {
     e.preventDefault();
@@ -187,7 +187,7 @@ function K13(e) {
     document.getElementById('k13').innerHTML = element;
 }
 
-document.getElementById('submit17').addEventListener('click',(e) => K14(e))
+document.getElementById('submit17').addEventListener('click',(e) => K14(e));
 
 function K14(e) {
     e.preventDefault();
@@ -199,7 +199,7 @@ function K14(e) {
     document.getElementById('k14').innerHTML = element;
 }
 
-document.getElementById('submit18').addEventListener('click',(e) => K15(e))
+document.getElementById('submit18').addEventListener('click',(e) => K15(e));
 
 function K15(e) {
     e.preventDefault();
@@ -211,7 +211,7 @@ function K15(e) {
     document.getElementById('k15').innerHTML = element;
 }
 
-document.getElementById('submit19').addEventListener('click',(e) => K16(e))
+document.getElementById('submit19').addEventListener('click',(e) => K16(e));
 
 function K16(e) {
     e.preventDefault();
@@ -221,4 +221,103 @@ function K16(e) {
     const d = ((+v * +v) - (+v0 * +v0)) / (2 * a);
     const element = " <div> <h4 class='blue'>Change in Distance:</h4><p class='blue'>" + d +" Meters</p></div>";
     document.getElementById('k16').innerHTML = element;
+}
+
+document.getElementById('submit20').addEventListener('click',(e) => K20(e));
+
+function K20(e) {
+    e.preventDefault();
+    const m = document.getElementById("coefficient20").value;
+    const fn = document.getElementById("normalforce20").value;
+    const ff = +m * +fn;
+    const element = " <div> <h4 class='blue'>Force of Friction:</h4><p class='blue'>" + ff +" Newtons</p></div>";
+    document.getElementById('k20').innerHTML = element;
+}
+
+document.getElementById('submit21').addEventListener('click',(e) => K21(e));
+
+function K21(e) {
+    e.preventDefault();
+    const ff = document.getElementById("frictionforce21").value;
+    const fn = document.getElementById("normalforce21").value;
+    const m = +ff / +fn;
+    const element = " <div> <h4 class='blue'>Coefficient of Friction:</h4><p class='blue'>" + m +" </p></div>";
+    document.getElementById('k21').innerHTML = element;
+}
+
+document.getElementById('submit22').addEventListener('click',(e) => K22(e));
+
+function K22(e) {
+    e.preventDefault();
+    const ff = document.getElementById("frictionforce22").value;
+    const m = document.getElementById("coefficient22").value;
+    const fn = +ff / +m;
+    const element = " <div> <h4 class='blue'>Normal Force:</h4><p class='blue'>" + fn +" Newtons</p></div>";
+    document.getElementById('k22').innerHTML = element;
+}
+
+document.getElementById('submit23').addEventListener('click',(e) => K23(e));
+
+function K23(e) {
+    e.preventDefault();
+    const v = document.getElementById("velocity23").value;
+    const r = document.getElementById("radius23").value;
+    const ac = (+v * +v) / r;
+    const element = " <div> <h4 class='blue'>Centripetal Acceleration:</h4><p class='blue'>" + ac +" m/s<sup>2</sup></p></div>";
+    document.getElementById('k23').innerHTML = element;
+}
+
+document.getElementById('submit24').addEventListener('click',(e) => K24(e));
+
+function K24(e) {
+    e.preventDefault();
+    const ac = document.getElementById("acceleration24").value;
+    const r = document.getElementById("radius24").value;
+    const v = Math.sqrt(+ac * +r);
+    const element = " <div> <h4 class='blue'>Linear Velocity:</h4><p class='blue'>" + v +" m/s</p></div>";
+    document.getElementById('k24').innerHTML = element;
+}
+
+document.getElementById('submit25').addEventListener('click',(e) => K25(e));
+
+function K25(e) {
+    e.preventDefault();
+    const ac = document.getElementById("acceleration25").value;
+    const v = document.getElementById("velocity25").value;
+    const r = (+v * +v) / ac;
+    const element = " <div> <h4 class='blue'>Radius:</h4><p class='blue'>" + r +" m</p></div>";
+    document.getElementById('k25').innerHTML = element;
+}
+
+document.getElementById('submit26').addEventListener('click',(e) => K26(e));
+
+function K26(e) {
+    e.preventDefault();
+    const m = document.getElementById("mass26").value;
+    const v = document.getElementById("velocity26").value;
+    const p = +m * +v;
+    const element = " <div> <h4 class='blue'>Momentum:</h4><p class='blue'>" + p +" (kg * m)/s</p></div>";
+    document.getElementById('k26').innerHTML = element;
+}
+
+document.getElementById('submit27').addEventListener('click',(e) => K27(e));
+
+function K27(e) {
+    e.preventDefault();
+    const p = document.getElementById("momentum27").value;
+    const v = document.getElementById("velocity27").value;
+    const m = +p / +v;
+    const element = " <div> <h4 class='blue'>Mass:</h4><p class='blue'>" + m +" kg</p></div>";
+    document.getElementById('k27').innerHTML = element;
+}
+
+document.getElementById('submit28').addEventListener('click',(e) => K28(e));
+
+function K28(e) {
+    e.preventDefault();
+    const p = document.getElementById("momentum28").value;
+    const m = document.getElementById("mass28").value;
+    const v = +p / +m;
+    const element = " <div> <h4 class='blue'>Velocity:</h4><p class='blue'>" + v +" m/s</p></div>";
+    document.getElementById('k28').innerHTML = element;
 }
